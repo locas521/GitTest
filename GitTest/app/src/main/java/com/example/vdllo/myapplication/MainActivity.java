4e825d8fa18718a828d3b79f0e6da78da51b9491
locas521 <locas521@outlook.com>
2016-03-28 16:27:11 +0800

    胡乃鑫 git测试

---
+++ GitTest/app/src/main/java/com/example/vdllo/myapplication/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.vdllo.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    //胡乃鑫 测试Git
+}
